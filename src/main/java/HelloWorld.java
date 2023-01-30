08933cb5624aa0b88a5caa5efabf75aca8da886f
AniratakDZ <stetter.fabian@gmx.de>
2023-01-30 10:34:35 +0100

    initial commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,17 @@
+/*  Created on 30.01.2023
+ *
+ *  Copyright (c) 2023
+ *  topsystem GmbH, Aachen, Germany
+ *
+ *  All rights reserved
+ */
+
+/**
+ * @author <a href="mailto:fabian.stetter@epg.com">Fabian Stetter</a>
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World :3");
+	}
+}
