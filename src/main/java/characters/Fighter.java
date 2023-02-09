690809a8a7ff4b303fc14b058e9c45967b78d5f5
Michael c <70096757+Serlunchalot@users.noreply.github.com>
2023-02-09 09:01:18 +0100

    Add files via upload

---
+++ src/main/java/characters/Fighter.java
@@ -0,0 +1,35 @@
+package main.characters;
+
+import main.Armor.Armor;
+import main.weapons.IWeaponBehaviour;
+
+public class Fighter extends Characters {
+
+
+
+    public Fighter(int strengh, int dexterity, int vitality, int intelligence, int wisdom, int charisma,
+                   IWeaponBehaviour mainhand, IWeaponBehaviour offhand, Armor armor) {
+
+        setStrengh(strengh);
+        setDexterity(dexterity);
+        setVitality(vitality);
+        setIntelligence(intelligence);
+        setWisdom(wisdom);
+        setCharisma(charisma);
+
+        setMainhand(mainhand);
+        setOffhand(offhand);
+        setArmor(armor);
+
+    }
+
+    @Override
+    void attack() {
+
+    }
+
+    @Override
+    void defend() {
+
+    }
+}
---
+++ src/main/java/characters/Wizzard.java
@@ -0,0 +1,30 @@
+package main.characters;
+
+import main.weapons.IWeaponBehaviour;
+
+public class Wizzard extends Characters {
+
+    public Wizzard(int strengh, int dexterity, int vitality, int intelligence, int wisdom, int charisma, IWeaponBehaviour mainhand, IWeaponBehaviour offhand) {
+
+        setStrengh(strengh);
+        setDexterity(dexterity);
+        setVitality(vitality);
+        setIntelligence(intelligence);
+        setWisdom(wisdom);
+        setCharisma(charisma);
+
+        setMainhand(mainhand);
+        setOffhand(offhand);
+    }
+
+
+    @Override
+    void attack() {
+
+    }
+
+    @Override
+    void defend() {
+
+    }
+}
