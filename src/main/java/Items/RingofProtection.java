5261835afbde8293f29a1b9ced37dbc081d00459
steffens_ced <cedric.steffens@fuckedup.de>
2023-02-16 08:20:09 +0100

    Item class

---
+++ src/main/java/Items/RingofProtection.java
@@ -0,0 +1,15 @@
+package Items;
+
+import characters.Characters;
+
+public class RingofProtection extends Rings {
+
+
+    public RingofProtection() {
+    }
+
+    @Override
+    public void useArtifact(Characters characters) {
+
+    }
+}
