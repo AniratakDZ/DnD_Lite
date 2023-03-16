067518946893a5c60adef0c7f8315a020f479bd8
ELEKTRUS1932 <111569366+ELEKTRUS1932@users.noreply.github.com>
2023-03-16 09:02:00 +0100

    zwischencommit

---
+++ src/main/java/Maps/Position.java
@@ -0,0 +1,17 @@
+package Maps;
+public class Position {
+
+    private int m_x = 0;
+    private int m_y = 0;
+
+    public Position(int x, int y) {
+
+    }
+
+    public int getX() {
+        return m_x;
+    }
+    public int getY() {
+        return m_y;
+    }
+}
